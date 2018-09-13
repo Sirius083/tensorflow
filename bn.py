f7bcb5a6c07da3e3be85dc13228c4b6be2c8175b
Sirius083 <1518246621@qq.com>
2018-09-13 08:34:27 +0800

    Update bn.py

--- bn.py
+++ bn.py
@@ -5,14 +5,19 @@
 # data = tf.zeros((10,200))
 # data2 = tf.layers.batch_normalization(inputs=data, axis = 1, momentum=0.997, epsilon=1e-5, center=True,scale=True)
 
-data = tf.zeros((10, 64, 64, 3)) # convolutional bn
-data1 = tf.layers.conv2d(data, filters=16, kernel_size=3,padding='same')
-data2 = tf.layers.batch_normalization(inputs=data1, axis = 1, momentum=0.997, epsilon=1e-5, center=True,scale=True)
+ # convolutional bn
+data = tf.zeros((10, 64, 64, 3))  # Note: tf.layers.conv2d 默认的data format是channel_last
+data1 = tf.layers.conv2d(data, filters=16, kernel_size=3,padding='same') # (10,64,64,16)
+
+# tf.layers.batch_normalization axis: 需要进行batch normalization的维度, conv中指的是feature map的维度
+#                                     channel first: axis = 1
+data2 = tf.layers.batch_normalization(inputs=data1, axis = 3, momentum=0.997, epsilon=1e-5, center=True,scale=True)
 init = tf.global_variables_initializer()
 with tf.Session() as sess:
     sess.run(init)
     allvar = tf.global_variables() # all global variables
     alltrainvar = tf.trainable_variables() # all trainable variables
+    print('data1',data1)
     # print all variables
     print('all variables =========== ')
     for var in allvar:
@@ -22,7 +27,6 @@ with tf.Session() as sess:
     for var in alltrainvar:
         print(var)
 
-
 '''
 all variables ===========
 <tf.Variable 'batch_normalization/gamma:0' shape=(64,) dtype=float32_ref>
@@ -38,13 +42,13 @@ all trainable variables ===========
 all variables ===========
 <tf.Variable 'conv2d/kernel:0' shape=(3, 3, 3, 16) dtype=float32_ref>
 <tf.Variable 'conv2d/bias:0' shape=(16,) dtype=float32_ref>
-<tf.Variable 'batch_normalization/gamma:0' shape=(64,) dtype=float32_ref>
-<tf.Variable 'batch_normalization/beta:0' shape=(64,) dtype=float32_ref>
-<tf.Variable 'batch_normalization/moving_mean:0' shape=(64,) dtype=float32_ref>
-<tf.Variable 'batch_normalization/moving_variance:0' shape=(64,) dtype=float32_ref>
+<tf.Variable 'batch_normalization/gamma:0' shape=(16,) dtype=float32_ref>
+<tf.Variable 'batch_normalization/beta:0' shape=(16,) dtype=float32_ref>
+<tf.Variable 'batch_normalization/moving_mean:0' shape=(16,) dtype=float32_ref>
+<tf.Variable 'batch_normalization/moving_variance:0' shape=(16,) dtype=float32_ref>
 all trainable variables ===========
 <tf.Variable 'conv2d/kernel:0' shape=(3, 3, 3, 16) dtype=float32_ref>
 <tf.Variable 'conv2d/bias:0' shape=(16,) dtype=float32_ref>
-<tf.Variable 'batch_normalization/gamma:0' shape=(64,) dtype=float32_ref>
-<tf.Variable 'batch_normalization/beta:0' shape=(64,) dtype=float32_ref>
+<tf.Variable 'batch_normalization/gamma:0' shape=(16,) dtype=float32_ref>
+<tf.Variable 'batch_normalization/beta:0' shape=(16,) dtype=float32_ref>
 '''
