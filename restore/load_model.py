1a6329f492fc6275bba732a39dda10fb94d147ae
jilizhen <lizhenjishx@gmail.com>
2019-02-19 14:49:44 +0800

    add load_model.py

---
+++ restore/load_model.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 16 11:25:53 2019
+
+@author: Sirius
+"""
+
+import numpy as np
+VGG_PATH = 'E:\\TF\\CNN-Visualization-master\\example\\model\\vgg\\vgg19.npy'
+data_dict = np.load(VGG_PATH, encoding='latin1').item()
+        
+
+
+
+
+
+
+        
+    
